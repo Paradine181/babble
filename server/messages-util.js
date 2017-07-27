8ccca8e87dd5f3033916064afab1ef33e3613ad4
Paradine181 <paradine181@gmail.com>
2017-07-28 01:03:47 +0300

    messages & users utils + tests

--- server/messages-util.js
+++ server/messages-util.js
@@ -1,7 +1,43 @@
-(function () {
+(function(global, factory) {
+    'use strict';
 
-  function addMessage(message) {
-      return;
-  }  
-
-}());
+    if (typeof module === 'object' && module.exports) {
+        module.exports = factory();
+    } else {
+        global.utils = factory();
+    }
+}(this, function() {
+    var messages = [];
+    var messageId = 0;
+    var lastRequestedId = 0;
+    return {
+        getMessages: function(number) {
+            var requiredMessages = []
+            for (var i = 0; i < Math.min(number + 1, messages.length); i++) {
+                if (messages[i].id > lastRequestedId) {
+                    requiredMessages.push({message: messages[i].message});
+                    lastRequestedId = messages[i].id;
+                }
+            }
+            return requiredMessages;
+        },
+        addMessage: function(message) {
+            messages.push({
+                id: ++messageId,
+                message: message.message
+            });
+            return messageId;
+        },
+        deleteMessage: function(id) {
+            for (i = messages.length - 1; i >= 0; i--) {
+                if (messages[i].id === id) {
+                    messages.splice(i, 1);
+                    return;
+                }
+            }
+        },
+        getMessagesCount: function() {
+            return messages.length;
+        }
+    };
+}));   
---
+++ server/users-util.js
@@ -0,0 +1,22 @@
+(function(global, factory) {
+    'use strict';
+
+    if (typeof module === 'object' && module.exports) {
+        module.exports = factory();
+    } else {
+        global.utils = factory();
+    }
+}(this, function() {
+    var users = 0;
+    return {
+        getUsers: function() {
+            return users;
+        },
+        addUser: function() {
+            return ++users;
+        },
+        deleteUser: function() {
+            return --users;
+        }
+    };
+}));   
